27fb86f6dc444589a51c57013dd0c4e6287aa254
William Swaney <williamswaney@Williams-MacBook-Pro.local>
2021-07-03 17:29:38 -0700

    Initial personalization of prelude

---
+++ personal/general.el
@@ -0,0 +1,3 @@
+(setq prelude-whitespace nil)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
---
+++ personal/parinfer.el
@@ -0,0 +1,4 @@
+(use-package parinfer-rust-mode
+  :hook emacs-lisp-mode clojure-mode
+  :init
+  (setq parinfer-rust-auto-download t))
