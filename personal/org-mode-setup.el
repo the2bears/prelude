c68a8d597fe0acb9b4ba7d2866ed2e71f8c55b66
William Swaney <williamswaney@Williams-MacBook-Pro.local>
2021-07-03 21:50:00 -0700

    Start of org-mode customization

---
+++ personal/org-mode-setup.el
@@ -0,0 +1,13 @@
+(use-package org
+  :defer t
+  :config
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t
+        org-src-fontify-natively t
+        org-fontify-quote-and-verse-blocks t
+        org-src-tab-acts-natively t
+        org-edit-src-content-indentation 2
+        org-hide-block-startup nil
+        org-src-preserve-indentation nil
+        org-startup-folded 'content
+        org-cycle-separator-lines 2))
